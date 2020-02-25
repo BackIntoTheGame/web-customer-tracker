6010988b731bbdbc29486b969133b98661823dea
BackIntoTheGame <sxlwork@outlook.com>
2020-02-25 17:03:26 +0800

    Spring & Hibernate for beginners section 30 Instead of getting data directly from DAO layer. adding a service lay.
    GetMapping and PostMapping

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,10 +1,11 @@
 package com.luv2code.springdemo.controller;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,13 +16,13 @@ public class CustomerController {
 
     // need to inject the customer dao
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel){
 
-        // get customers from the dao
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> theCustomers = customerService.getCustomers();
 
         // add the customers to the model
         theModel.addAttribute("customers", theCustomers);
